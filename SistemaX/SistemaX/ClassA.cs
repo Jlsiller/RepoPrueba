64568236cebd2cfec9f51a33ec2fdb4ff08e3b86
Jose Luis Siller Lopez <jose.luis.siller@pemex.com>
2020-08-15 23:01:03 -0500

    cambio classA en rama Master

---
+++ SistemaX/SistemaX/ClassA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaX
+{
+    public class ClassA
+    {
+        public int MyProperty { get; set; }
+    }
+}
